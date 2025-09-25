fc8436ccd94b041f5ca88813c4438787e5155ff6
iranzi17 <50293498+iranzi17@users.noreply.github.com>
2025-09-25 18:49:00 +0200

    Fix Application plugin registrant for Flutter embedding v2

--- android/app/src/main/kotlin/com/iranzipjc/gfcallapp/Application.kt
+++ android/app/src/main/kotlin/com/iranzipjc/gfcallapp/Application.kt
@@ -1,17 +1,18 @@
 package com.iranzipjc.gfcallapp
 
 import dev.fluttercommunity.plus.androidalarmmanager.AndroidAlarmManagerPlugin
-import io.flutter.app.FlutterApplication
-import io.flutter.plugin.common.PluginRegistry
+import io.flutter.embedding.android.FlutterApplication
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-class Application : FlutterApplication(), PluginRegistry.PluginRegistrantCallback {
+class Application : FlutterApplication(), PluginRegistrantCallback {
     override fun onCreate() {
         super.onCreate()
         AndroidAlarmManagerPlugin.setPluginRegistrant(this)
     }
 
-    override fun registerWith(registry: PluginRegistry) {
-        GeneratedPluginRegistrant.registerWith(registry)
+    override fun registerWith(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 }
